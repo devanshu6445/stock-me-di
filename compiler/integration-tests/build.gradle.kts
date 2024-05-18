0cbb8de37f29b581e85b8b9cddd295e23a3fd2af
Devanshu Pathsariya <devanshu.pathsariya@pw.live>
2024-05-18 22:25:18 +0530

    Fixed: Ir not being dumped correctly by ir dumping plugin

--- compiler/integration-tests/build.gradle.kts
+++ compiler/integration-tests/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.android.build.gradle.internal.tasks.factory.dependsOn
-
 plugins {
   alias(libs.plugins.org.jetbrains.kotlin.jvm)
   alias(libs.plugins.ksp)
@@ -15,9 +13,6 @@ tasks.withType<Test> {
 kotlin {
   compilerOptions {
 //        languageVersion.set(KotlinVersion.KOTLIN_2_0)
-    freeCompilerArgs.add(
-        "-Xplugin=${project(":compiler:kcp").projectDir}/build/libs/kcp-1.0.0.jar"
-    )
 
     freeCompilerArgs.addAll(
       listOf(
@@ -28,11 +23,11 @@ kotlin {
   }
 }
 
-// always run a clean build, because after applying compiler plugin
-// ksp generated classes are not resolved todo need to resolve the issue
-afterEvaluate {
-  tasks.named("kspKotlin").dependsOn("clean")
-}
+//// always run a clean build, because after applying compiler plugin
+//// ksp generated classes are not resolved todo need to resolve the issue
+//afterEvaluate {
+//  tasks.named("kspKotlin").dependsOn("clean")
+//}
 
 dependencies {
   implementation(projects.runtime)
@@ -45,8 +40,7 @@ dependencies {
   testImplementation(libs.ksp.testing)
   testImplementation(libs.koTest)
 
-  // to depend this project onto kotlin-di-compiler so that jar is generated every time this project is compiled
-  implementation(projects.compiler.kcp)
+  kotlinCompilerPluginClasspath(projects.compiler.kcp)
 }
 
 application {
