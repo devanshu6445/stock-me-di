d83ba7a9c81f01b416e4e05c1b2044ee097cf8cb
Devanshu Pathsariya <devanshu6445@gmail.com>
2024-06-20 23:42:54 +0530

    Initial setup of lint ci

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,49 +1,63 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
-    repositories {
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven {
-            url =
-                java.net.URI.create("https://maven.pkg.jetbrains.space/stockme/p/main/stock-me-android")
-            val properties = java.util.Properties().apply {
-                load(java.io.FileInputStream(File("${rootDir.absolutePath}/local.properties")))
-            }
-            credentials {
-                username = (properties["spaceUsername"] ?: "") as String
-                password = (properties["spaceToken"] ?: "") as String
-            }
-        }
-        maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
-        google()
-        mavenCentral()
-        mavenLocal()
-        gradlePluginPortal()
-    }
+	repositories {
+		maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+		maven {
+			url =
+				java.net.URI.create("https://maven.pkg.jetbrains.space/stockme/p/main/stock-me-android")
+
+			val usernameConst = "spaceUsername"
+			val token = "spaceToken"
+			val properties = java.util.Properties().apply {
+				try {
+					load(java.io.FileInputStream(File("${rootDir.absolutePath}/local.properties")))
+				} catch (e: java.io.FileNotFoundException) {
+					java.util.Properties()
+				}
+			}
+			credentials {
+				username = (properties[usernameConst] ?: "") as String
+				password = (properties[token] ?: "") as String
+			}
+		}
+		maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+		google()
+		mavenCentral()
+		mavenLocal()
+		gradlePluginPortal()
+	}
 }
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+	id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 dependencyResolutionManagement {
-    // Do RCA and find alternative and elegant approach
-    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
-    repositories {
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven {
-            url =
-                java.net.URI.create("https://maven.pkg.jetbrains.space/stockme/p/main/stock-me-android")
-            val properties = java.util.Properties().apply {
-                load(java.io.FileInputStream(File("${rootDir.absolutePath}/local.properties")))
-            }
-            credentials {
-                username = (properties["spaceUsername"] ?: "") as String
-                password = (properties["spaceToken"] ?: "") as String
-            }
-        }
-        google()
-        mavenCentral()
-        mavenLocal()
-    }
+	// Do RCA and find alternative and elegant approach
+	repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+	repositories {
+		maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+		maven {
+			url =
+				java.net.URI.create("https://maven.pkg.jetbrains.space/stockme/p/main/stock-me-android")
+
+			val usernameConst = "spaceUsername"
+			val token = "spaceToken"
+			val properties = java.util.Properties().apply {
+				try {
+					load(java.io.FileInputStream(File("${rootDir.absolutePath}/local.properties")))
+				} catch (e: java.io.FileNotFoundException) {
+					java.util.Properties()
+				}
+			}
+			credentials {
+				username = (properties[usernameConst] ?: "") as String
+				password = (properties[token] ?: "") as String
+			}
+		}
+		google()
+		mavenCentral()
+		mavenLocal()
+	}
 }
 
 include(":runtime")
