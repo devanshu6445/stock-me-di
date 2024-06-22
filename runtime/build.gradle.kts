cd7a2b0312e854ebe786e79b93ba7e914412ac94
Devanshu Pathsariya <devanshu6445@gmail.com>
2024-06-23 01:25:27 +0530

    Upgraded kotlin-inject dependency and fixed ksp target issue

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -1,18 +1,22 @@
+import org.gradle.configurationcache.extensions.capitalized
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
+
 plugins {
   alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.ksp)
-    `maven-publish`
+  alias(libs.plugins.ksp)
+  `maven-publish`
 }
 
 group = "in.stock.me"
 version = "1.0.0-SNAPSHOT"
 
 publishing {
-    publications {
-        withType<MavenPublication> {
-          artifactId = "di-runtime" + artifactId.replace(project.name, "")
-        }
+  publications {
+    withType<MavenPublication> {
+      artifactId = "di-runtime" + artifactId.replace(project.name, "")
     }
+  }
 }
 
 kotlin {
@@ -24,10 +28,22 @@ kotlin {
   iosX64()
   iosSimulatorArm64()
   jvm()
+
+  sourceSets {
+    commonMain {
+      dependencies {
+        implementation(libs.kotlin.inject.runtime)
+      }
+    }
+  }
 }
 
 dependencies {
-  kotlin.targets.all {
-    add("ksp${this.name}", libs.kotlin.inject.compiler)
+  kotlin.targets.filterIsInstance<KotlinNativeTarget>().forEach {
+    add("ksp${it.name.capitalized()}", libs.kotlin.inject.compiler)
+  }
+
+  kotlin.targets.filterIsInstance<KotlinJvmTarget>().forEach {
+    add("ksp${it.name.capitalized()}", libs.kotlin.inject.compiler)
   }
 }
