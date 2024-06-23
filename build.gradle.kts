6f10496e3369810f6f0bb5033fb6967e66775213
Devanshu Pathsariya <devanshu6445@gmail.com>
2024-06-23 13:41:18 +0530

    Added cache version and changed cache key to libs.versions.toml file as the build gradle changes all the time

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,31 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.stock.me.linter)
+    base
 }
+
+val testReport by tasks.registering(TestReport::class) {
+    destinationDirectory = layout.buildDirectory.map { it.asFile.resolve("reports") }
+}
+
+val copyTestResults by tasks.registering(Copy::class) {
+    destinationDir = layout.buildDirectory.get().asFile.resolve("test-results")
+    includeEmptyDirs = false
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+
+val testReportApple by tasks.registering(TestReport::class) {
+    destinationDirectory = layout.buildDirectory.map { it.asFile.resolve("reports") }
+}
+
+val copyTestResultsApple by tasks.registering(Copy::class) {
+    destinationDir = layout.buildDirectory.get().asFile.resolve("test-results")
+    includeEmptyDirs = false
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+
+val check: Task by tasks.getting
+val checkApple: Task by tasks.creating
+
+check.finalizedBy(testReport, copyTestResults)
+checkApple.finalizedBy(testReportApple, copyTestResultsApple)
