1677540fcc971f93f40f99a65f4790efe000efcd
Devanshu Pathsariya <devanshu6445@gmail.com>
2024-08-04 01:08:58 +0530

    Removed unnecessary gradle plugin[di-compiler]
    Signed-off-by: Devanshu Pathsariya <devanshu6445@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kmp.convention) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.di.compiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.stock.me.linter)
