ead068433d3622ab778888fb267a81d1586463be
Restyled.io <commits@restyled.io>
2024-08-04 05:30:56 +0000

    Restyled by shfmt

--- scripts/deploy_to_remote_repo.sh
+++ scripts/deploy_to_remote_repo.sh
@@ -1,9 +1,9 @@
 echo "Publish compiler plugin"
 
 if [ ! -f "settings.gradle.kts" ]; then
-    echo "ERROR:"
-    echo "Please run this script from the root directory of the repo."
-    exit 1
+  echo "ERROR:"
+  echo "Please run this script from the root directory of the repo."
+  exit 1
 fi
 
 gradle :compiler:core:publish
--- scripts/dev_test.sh
+++ scripts/dev_test.sh
@@ -1,9 +1,9 @@
 echo "Dev test repo"
 
 if [ ! -f "settings.gradle.kts" ]; then
-    echo "ERROR:"
-    echo "Please run this script from the root directory of the repo."
-    exit 1
+  echo "ERROR:"
+  echo "Please run this script from the root directory of the repo."
+  exit 1
 fi
 
 gradle :compiler:integration-tests:compileKotlin
